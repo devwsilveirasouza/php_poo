02767c0952afdaa637b0d99fa06ed63113a0b978
devwsilveirasouza <dev.wsilveirasouza@gmail.com>
2022-10-24 14:16:01 -0300

    Métodos e Atributos estáticos

--- poo/Aulas/11-metodos-atributos-estaticos.php
+++ poo/Aulas/11-metodos-atributos-estaticos.php
@@ -1,1 +1,28 @@
 <?php
+
+class Login {
+
+    public static $user;
+
+    public static function verificaLogin() {
+        /** $this não funciona com parâmetros státicos 
+         * somente o self::$user funciona com parâmetros státicos
+        */
+        echo "O usuário ".self::$user." está logado!";
+    }
+
+    public function sairSistema() {
+        echo "O usuário deslogou!";
+    }
+}
+/** Quando utilizamos métodos estáticos não é preciso mais instânciar a classe 
+ * Basta declarar o nome da classe + :: e o método
+*/
+
+Login::$user = "Admin";
+Login::verificaLogin();
+
+echo "<hr>";
+
+$login = new Login();
+$login->sairSistema();
